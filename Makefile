6be2a05361705ed05dc7d3aa34f144ce99b62b85
Mohamed-Amine Elammari <moelamma@e1r10p12.1337.ma>
2026-05-24 18:25:32 +0100

    codexion

---
+++ Makefile
@@ -0,0 +1,26 @@
+NAME	= codexion
+CC		= cc
+FLAGS	= -Wall -Wextra -Werror -pthread
+
+SRCS	= srcs/main.c \
+			srcs/parse_args.c
+
+OBJS	= $(SRCS:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	$(CC) $(FLAGS) $(OBJS) -o $(NAME)
+
+%.o: %.c
+	$(CC) $(FLAGS) -I includes -c $< -o $@
+
+clean:
+	rm -f $(OBJS)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ includes/codexion.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   codexion.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moelamma <moelamma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/23 15:55:21 by moelamma          #+#    #+#             */
+/*   Updated: 2026/05/23 16:01:23 by moelamma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CODEXION_H
+# define CODEXION_H
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+typedef struct s_args
+{
+	int		coders;
+	long	time_to_burnout;
+	long	time_to_compile;
+	long	time_to_debug;
+	long	time_to_refactor;
+	int		nb_compiles_required;
+	long	dongle_cooldown;
+	int		scheduler;
+}	t_args;
+
+#endif
