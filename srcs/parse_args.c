6ce11fb0694956e62f3f3ea34536d186c80b4976
Mohamed-Amine Elammari <moelamma@e2r10p12.1337.ma>
2026-06-19 14:33:57 +0100

    updated

--- srcs/parse_args.c
+++ srcs/parse_args.c
@@ -6,10 +6,9 @@
 /*   By: moelamma <moelamma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/23 16:44:34 by moelamma          #+#    #+#             */
-/*   Updated: 2026/06/06 22:48:42 by moelamma         ###   ########.fr       */
+/*   Updated: 2026/06/19 14:33:40 by moelamma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "../includes/codexion.h"
 
@@ -27,12 +26,12 @@ int	ft_atoi_strict(const char *s, long *out)
 	i = 0;
 	if (!s || !s[i])
 		return (0);
-	while(s[i])
+	while (s[i])
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			if (result > (LONG_MAX - (s[i] - '0')) / 10)
-    			return (0);
+				return (0);
 			result = result * 10 + (s[i] - '0');
 		}
 		else
@@ -43,40 +42,38 @@ int	ft_atoi_strict(const char *s, long *out)
 	return (1);
 }
 
-
-
 int	parse_args(int argc, char **argv, t_args *args)
 {
-    long    tmp;
+	long	tmp;
 
-    if (argc != 9)
-        return (fprintf(stderr, "Error: expected 8 arguments\n"), 0);
-    if (!ft_atoi_strict(argv[1], &tmp) || tmp < 1)
-        return (fprintf(stderr, "Error: invalid number_of_coders\n"), 0);
-    args->nb_coders = (int)tmp;
-    if (!ft_atoi_strict(argv[2], &tmp) || tmp < 1)
-        return (fprintf(stderr, "Error: invalid time_to_burnout\n"), 0);
-    args->time_to_burnout = tmp;
-    if (!ft_atoi_strict(argv[3], &tmp))
-        return (fprintf(stderr, "Error: invalid time_to_compile\n"), 0);
-    args->time_to_compile = tmp;
-    if (!ft_atoi_strict(argv[4], &tmp))
-        return (fprintf(stderr, "Error: invalid time_to_debug\n"), 0);
-    args->time_to_debug = tmp;
-    if (!ft_atoi_strict(argv[5], &tmp))
-        return (fprintf(stderr, "Error: invalid time_to_refactor\n"), 0);
-    args->time_to_refactor = tmp;
-    if (!ft_atoi_strict(argv[6], &tmp) || tmp < 1)
-        return (fprintf(stderr, "Error: invalid number_of_compiles_required\n"), 0);
-    args->nb_compiles_required = (int)tmp;
-    if (!ft_atoi_strict(argv[7], &tmp))
-        return (fprintf(stderr, "Error: invalid dongle_cooldown\n"), 0);
-    args->dongle_cooldown = tmp;
-    if (strcmp(argv[8], "fifo") == 0)
-        args->scheduler = 0;
-    else if (strcmp(argv[8], "edf") == 0)
-        args->scheduler = 1;
-    else
-        return (fprintf(stderr, "Error: scheduler must be 'fifo' or 'edf'\n"), 0);
-    return (1);
+	if (argc != 9)
+		return (fprintf(stderr, "Error: expected 8 arguments\n"), 0);
+	if (!ft_atoi_strict(argv[1], &tmp) || tmp < 1)
+		return (fprintf(stderr, "Error: invalid number_of_coders\n"), 0);
+	args->nb_coders = (int)tmp;
+	if (!ft_atoi_strict(argv[2], &tmp) || tmp < 1)
+		return (fprintf(stderr, "Error: invalid time_to_burnout\n"), 0);
+	args->time_to_burnout = tmp;
+	if (!ft_atoi_strict(argv[3], &tmp))
+		return (fprintf(stderr, "Error: invalid time_to_compile\n"), 0);
+	args->time_to_compile = tmp;
+	if (!ft_atoi_strict(argv[4], &tmp))
+		return (fprintf(stderr, "Error: invalid time_to_debug\n"), 0);
+	args->time_to_debug = tmp;
+	if (!ft_atoi_strict(argv[5], &tmp))
+		return (fprintf(stderr, "Error: invalid time_to_refactor\n"), 0);
+	args->time_to_refactor = tmp;
+	if (!ft_atoi_strict(argv[6], &tmp) || tmp < 1)
+		return (fprintf(stderr, "Error: invalid number_of_compiles_required\n"), 0);
+	args->nb_compiles_required = (int)tmp;
+	if (!ft_atoi_strict(argv[7], &tmp))
+		return (fprintf(stderr, "Error: invalid dongle_cooldown\n"), 0);
+	args->dongle_cooldown = tmp;
+	if (strcmp(argv[8], "fifo") == 0)
+		args->scheduler = 0;
+	else if (strcmp(argv[8], "edf") == 0)
+		args->scheduler = 1;
+	else
+		return (fprintf(stderr, "Error: scheduler must be 'fifo' or 'edf'\n"), 0);
+	return (1);
 }
